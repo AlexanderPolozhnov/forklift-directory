8525933e07a61d26128cf12184dad1e323fe40f3
Alexander Polozhnov <polozhnov.alex@gmail.com>
2026-05-07 15:01:29 +0300

    chore: initialize Spring Boot project with Maven

---
+++ src/main/java/com/alexanderpolozhnov/forklift_directory/ForkliftDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.alexanderpolozhnov.forklift_directory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForkliftDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForkliftDirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexanderpolozhnov/forklift_directory/ForkliftDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexanderpolozhnov.forklift_directory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForkliftDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
